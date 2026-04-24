ddbb1d454109721215e89c589e615447846884c6
Jose <jorgejose331gmail.com>
2026-04-24 19:47:11 -0300

    punto 3 y 4 terminado

--- ToDo.c
+++ ToDo.c
@@ -21,6 +21,7 @@ typedef struct Nodo {
 //FUNCIONES
 Nodo *CrearListaVacia();
 Nodo *CargarTareas(int ID);
+void TransferirTareas(Nodo **pendientes, Nodo **realizadas);
 void MostrarLista(Nodo *lista);
 
 int main(){
@@ -44,7 +45,11 @@ int main(){
 
     printf("\n\nLISTADO DE TAREAS PENDIENTES:");
     MostrarLista(listaPendientes);
-
+    TransferirTareas(&listaPendientes,&listaRealizadas);
+    printf("\n\nLISTADO DE TAREAS REALIZADAS:");
+    MostrarLista(listaRealizadas);
+    printf("\n\nLISTADO DE TAREAS PENDIENTES:");
+    MostrarLista(listaPendientes);
     return 0;
 }
 
@@ -86,3 +91,51 @@ void MostrarLista(Nodo *lista){
         i++;
     }
 }
+
+void TransferirTareas(Nodo **pendientes, Nodo **realizadas) {
+    // 'actual' es el iterador de la lista de pendientes
+    Nodo *actual = *pendientes;  
+    
+    // 'anterior' mantiene la referencia al nodo previo para permitir la eliminación en O(1)
+    Nodo *anterior = NULL;       
+    int respuesta;
+
+    while (actual != NULL) {
+        printf("\nID: %d | Descripcion: %s", actual->T.TareaID, actual->T.Descripcion);
+        printf("\n Realizo esta tarea? (1: SI / 0: NO): ");
+        scanf("%d", &respuesta);
+
+        if (respuesta == 1) {
+            // --- 1. EXTRACCIÓN DEL NODO DE LA LISTA ORIGEN ---
+            
+            // Referenciamos el nodo que será extraído para evitar la pérdida de su dirección
+            Nodo *auxMover = actual; 
+
+            if (anterior == NULL) {
+                /* CASO BORRADO EN CABECERA */
+                *pendientes = actual->Siguiente;
+            } else {
+                /* CASO BORRADO INTERMEDIO/FINAL */
+                anterior->Siguiente = actual->Siguiente;
+            }
+
+            
+            actual = actual->Siguiente;
+
+            // --- 2. INSERCIÓN DEL NODO EN LA LISTA DESTINO ---
+            
+            auxMover->Siguiente = *realizadas;
+
+            
+            *realizadas = auxMover;
+
+            printf(">> Nodo reubicado en la lista de realizadas.\n");
+
+        } else {
+            
+            anterior = actual;
+            actual = actual->Siguiente;
+        }
+    }
+}
+
